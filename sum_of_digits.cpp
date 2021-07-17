2e7d093418e4a432220a9f17a1bb6faa4f0fbfd7
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-07-18 01:57:24 +0600

    Add files via upload

---
+++ sum_of_digits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        int sum = 0;
+        while(n>0) {
+            int c = n % 10;
+            sum += c;
+            n = n/ 10;
+        }
+        cout << sum << endl;
+    }
+
+}
