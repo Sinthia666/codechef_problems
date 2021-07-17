ea6b61b4f559d723b5820066d644d587685d8c9c
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-07-18 01:35:05 +0600

    Add files via upload

---
+++ add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+   int t; cin >> t;
+   int c;
+   while(t--) {
+       int a; int b; cin >> a >> b;
+       
+       c = a+b;
+       cout << c << endl;
+       
+   }
+    // cout << c << endl;
+}
