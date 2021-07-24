0ee86e327c74071b7e5c7b0b3688c3814ed66e86
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-07-24 12:54:52 +0600

    Add files via upload

---
+++ finding_square_root.py
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int t,s;
+    while(t--) {
+     int x;
+     cin >> x;
+     int s = sqrt(x);
+     cout << s << endl;
+}
+    // cout << s << endl;
+}
