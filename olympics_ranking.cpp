f8fe4a9ea0577c4615b05b86d2d248ee1dc0ae07
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-08-06 16:36:31 +0600

    Add files via upload

---
+++ olympics_ranking.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main() {
+    int t; cin >> t;
+    while(t--) {
+    int a1, a2, a3, b1, b2,b3;
+    cin >> a1 >> a2 >> a3 >> b1 >> b2 >> b3;
+    int c = a1+a2+a3;
+    int d = b1 + b2 + b3;
+    if(c>d)cout << "1" << endl;
+    else cout << "2" << endl;
+
+
+    }
+    
+    
+}
+ 
